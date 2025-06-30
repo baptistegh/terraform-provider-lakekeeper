9a4473e29f708dd8bd3b3534ed124b8e0fd781d8
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2025-06-30 21:49:36 +0200

    feat(test): add acceptance test on lakekeeper_default_project datasource (#12)

---
+++ internal/provider/datasource_lakekeeper_default_project_test.go
@@ -0,0 +1,25 @@
+//go:build acceptance
+// +build acceptance
+
+package provider
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccDataLakekeeperDefaultProject_basic(t *testing.T) {
+	resource.ParallelTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: `data "lakekeeper_default_project" "foo" {}`,
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr("data.lakekeeper_default_project.foo", "id", "00000000-0000-0000-0000-000000000000"),
+					resource.TestCheckResourceAttr("data.lakekeeper_default_project.foo", "name", "Default Project"),
+				),
+			},
+		},
+	})
+}
