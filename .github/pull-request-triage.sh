044a6ce12b4cfe85e9e020c634b95f7f834d066d
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2025-07-06 19:42:01 +0200

    ci: remove feature label (#46)

--- .github/pull-request-triage.sh
+++ .github/pull-request-triage.sh
@@ -13,11 +13,21 @@ REGEX="^(build|chore|ci|docs|feat|fix|perf|refactor|style|test)(\([^)]+\))?(!)?:
 
 if [[ "$PR_TITLE" =~ $REGEX ]]; then
     echo "✅ PR title follows Conventional Commit format."
+    gh pr edit "$PR_NUMBER" --repo "$REPO" --remove-label "invalid-title"
 else
-    # Leave a comment on the PR
-    gh pr comment "$PR_NUMBER" --repo "$REPO" --body ":warning: The title of this PR does not follow the [Conventional Commit](https://www.conventionalcommits.org/) format.  
+    # Tag invalid title
+    LABEL_EXISTS=$(gh pr view "$PR_NUMBER" --repo "$REPO" --json labels --jq '.labels[].name | select(. == "invalid-title")')
+
+    if [ -z "$LABEL_EXISTS" ]; then
+         # Leave a comment on the PR
+        gh pr comment "$PR_NUMBER" --repo "$REPO" --body ":warning: The title of this PR does not follow the [Conventional Commit](https://www.conventionalcommits.org/) format.  
 
 Expected format: \`type(scope?): description\`, e.g. \`feat(login): add new login page\`"
+
+        gh pr edit $PR_NUMBER --add-label "invalid-title"
+    else
+        echo "Label invalid-title present, skipping comment."
+    fi
 
     exit 1
 fi
@@ -43,5 +53,17 @@ fi
 if [[ "$TYPE" == "fix" ]]; then
     echo "🏷️ Added label: bug"
     gh pr edit "$PR_NUMBER" --repo "$REPO" --add-label "bug"
+    exit 0
+fi
+
+if [[ "$TYPE" == "docs" ]]; then
+    echo "🏷️ Added label: documentation"
+    gh pr edit "$PR_NUMBER" --repo "$REPO" --add-label "documentation"
+    exit 0
+fi
+
+if [[ "$TYPE" == "ci" ]]; then
+    echo "🏷️ Added label: ci"
+    gh pr edit "$PR_NUMBER" --repo "$REPO" --add-label "ci"
     exit 0
 fi
