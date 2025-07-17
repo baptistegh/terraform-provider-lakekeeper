1729f598f342e53942e1c164b587c6aab8f8adba
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2025-07-17 17:33:54 +0200

    feat: add role (role/user) assignments (#104)

---
+++ examples/resources/lakekeeper_role_user_assignment/resource.tf
@@ -0,0 +1,5 @@
+resource "lakekeeper_role_user_assignment" "data_analysts" {
+  role_id     = "a4653498-1dd9-4f12-a2e4-1cc7d4023226"
+  user_id     = "cb6ee351-68ff-4299-87f2-876964f6d8dd"
+  assignments = ["assignee"]
+}
