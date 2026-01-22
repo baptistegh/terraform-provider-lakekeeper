91d79d86494f7c0d8857db9726fe55ba0a5ca2a4
Baptiste Gouhoury <gouhoury.baptiste@gmail.com>
2026-01-22 11:28:25 +0100

    chore(ci): use import in tools package

--- tools/tools.go
+++ tools/tools.go
@@ -2,4 +2,8 @@
 
 package tools
 
+import (
+	_ "github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs"
+)
+
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs generate -provider-dir ..
